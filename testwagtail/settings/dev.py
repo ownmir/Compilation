3a69689e1b7819821ccd055fa796fb578aadeef2
Volodymyr Fomin <ownmir@i.ua>
2018-12-14 23:19:52 +0200

    python-decouple dj-database-url 14122018

--- testwagtail/settings/dev.py
+++ testwagtail/settings/dev.py
@@ -1,10 +1,11 @@
 from .base import *
+from decouple import config
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u77x*r&rs*(mksy$k+m606%x6b*46th$xq(_$o$0h2g9)u^x*i'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*'] 
