42216c63640bb1df34018d8ea8aa88afcab6768f
Volodymyr Fomin <ownmir@i.ua>
2018-12-21 11:42:43 +0200

    ver0.1.3 urls translate first; fix links

--- testwagtail/urls.py
+++ testwagtail/urls.py
@@ -29,7 +29,8 @@ urlpatterns = [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
-    url(r'', include(wagtail_urls)),
+    url(r'', include(translate_tutor_urls)),
+    url(r'^wt/', include(wagtail_urls)),
 
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
