9783831e49d319144ffc36dfedac04caed7794be
Volodymyr Fomin <ownmir@i.ua>
2018-12-24 22:31:18 +0200

    'ver0.1.4Markdown'

--- testwagtail/settings/base.py
+++ testwagtail/settings/base.py
@@ -16,9 +16,9 @@ from decouple import config
 import dj_database_url
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print('PROJECT_DIR', PROJECT_DIR)
+# print('PROJECT_DIR', PROJECT_DIR)
 BASE_DIR = os.path.dirname(PROJECT_DIR)
-print('BASE_DIR', BASE_DIR)
+# print('BASE_DIR', BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
